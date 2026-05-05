71a6f317cab592e0fa3e04aa0ae9b44c7b70f7d3
kaushikacatering <kaushikacatering@users.noreply.github.com>
2026-05-05 19:17:52 +0530

    debug: add entry-point timing to admin/index.php

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,5 @@
 <?php
+error_log('ENTRY: admin/index.php hit at ' . date('H:i:s') . ' | METHOD: ' . $_SERVER['REQUEST_METHOD'] . ' | URI: ' . $_SERVER['REQUEST_URI'] . ' | Content-Length: ' . ($_SERVER['CONTENT_LENGTH'] ?? 'none'));
 // Version
 define('VERSION', '3.0.3.8');
 ini_set('session.gc_maxlifetime', 864000); 
